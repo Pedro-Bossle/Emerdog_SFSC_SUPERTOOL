d5cbca533df1c058becc98fd78a115e94b43ea1b
Pedro Bossle <pedro.bossle.s@gmail.com>
2026-04-10 13:48:57 -0300

    Struct

---
+++ my-app/src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Home.css"
+
+const Home = () => {
+    return (
+        <div className='home'>
+            <h1>Formulário de procedimentos em meu perfil</h1>
+        </div>
+
+    )
+}
+
+export default Home;
