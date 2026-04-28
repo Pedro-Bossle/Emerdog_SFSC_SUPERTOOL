ec5811b4a801df58e47bb56f03c85fe0e107d4b0
Pedro Bossle <pedro.bossle.s@gmail.com>
2026-04-28 11:17:59 -0300

    .

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Emerdog_SFSC_SUPERTOOL/',
+  base: process.env.VERCEL ? '/' : '/Emerdog_SFSC_SUPERTOOL/',
   plugins: [react()],
 })
