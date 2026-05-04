76042bc14a882392abf8e3cc47fcc0d6b04b73a7
Pedro Bossle <pedro.bossle.s@gmail.com>
2026-05-04 09:13:25 -0300

    Importado a correção

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './pages/Supertabela/Supertabela_cidades/Supertabelacidades.css'
+import './pages/Supertabela/Supertabela_planos/Supertabelaplanos.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
