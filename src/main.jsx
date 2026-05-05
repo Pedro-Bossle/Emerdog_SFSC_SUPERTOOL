2079b8f4fc7e4e25da13375bc9d0bf25a5cf092c
Pedro Bossle <pedro.bossle.s@gmail.com>
2026-05-05 17:11:17 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,8 @@ import './pages/Supertabela/Supertabela_cidades/Supertabelacidades.css'
 import './pages/Supertabela/Supertabela_planos/Supertabelaplanos.css'
 import './pages/Supertabela/Supertabela_procedimentos/Supertabelaprocedimentos.css'
 import './pages/Supertabela/Supertabela_negociacoes/Supertabelanegociacoes.css'
+import './pages/Credenciamento/Credenciamento_main/Credenciamento_main.css'
+import './pages/Credenciamento/Credenciamento_doc/Credenciamento_doc.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
